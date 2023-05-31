cdd168c5f003bfc8a6eb394b5c391e9915b7fbcd
AyimWilliam <odhisayim@gmail.com>
2023-05-31 08:07:05 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,8 +1,77 @@
 import sqlite3
 
-CONN = sqlite3.connect('lib/dogs.db')
+CONN = sqlite3.connect('dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    
-    pass
+    def __init__(self, name=None, breed=None):
+        self.id = None
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute("CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)")
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS dogs")
+        CONN.commit()
+
+    def save(self):
+        if self.id is None:
+            CURSOR.execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", (self.name, self.breed))
+            self.id = CURSOR.lastrowid
+        else:
+            CURSOR.execute("UPDATE dogs SET name=?, breed=? WHERE id=?", (self.name, self.breed, self.id))
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = Dog(name, breed)
+        dog.save()
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        dog = Dog(row[1], row[2])
+        dog.id = row[0]
+        return dog
+
+    @classmethod
+    def get_all(cls):
+        CURSOR.execute("SELECT * FROM dogs")
+        rows = CURSOR.fetchall()
+        return [cls.new_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_name(cls, name):
+        CURSOR.execute("SELECT * FROM dogs WHERE name=?", (name,))
+        row = CURSOR.fetchone()
+        if row is not None:
+            return cls.new_from_db(row)
+        return None
+
+    @classmethod
+    def find_by_id(cls, dog_id):
+        CURSOR.execute("SELECT * FROM dogs WHERE id=?", (dog_id,))
+        row = CURSOR.fetchone()
+        if row is not None:
+            return cls.new_from_db(row)
+        return None
+
+    # Bonus methods
+
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = cls.find_by_name(name)
+        if dog is not None:
+            return dog
+        return cls.create(name, breed)
+
+    def update(self):
+        self.save()
+
+    def __repr__(self):
+        return f"Dog(id={self.id}, name='{self.name}', breed='{self.breed}')"
